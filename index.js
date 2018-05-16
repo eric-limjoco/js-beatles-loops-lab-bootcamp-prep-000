770d0b228c6f7638c73ac459627c96c484baaba7
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 11:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-// + Create a function `theBeatlesPlay`, which accepts two parameters- an array of musicians and an array of instruments. The body of the function should create an empty array stored in a variable. The function should also contain a for loop which loops over the array of musicians. You'll want to be careful about what value you set your counter variable to store. (Hint: Think about what the first index of an array is). The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: `"John Lennon plays guitar"`. This string should be added to the empty array you created. The loop should make the same sentence for every member of the musicians array. The function should return the array of new strings.
-
 // + Create a function `johnLennonFacts`. This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
 
 // ```js
@@ -21,6 +17,14 @@ function theBeatlesPlay(musicians, instruments){
   var arr = []
   for(var i = 0; i < musicians.length; i++){
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+  var arr = []
+  while(facts.length > 0){
+    arr.push(facts.shift()+"!!!")
   }
   return arr
 }
